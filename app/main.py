484f0b84976137001ea7590e9538b4b40607db48
galilei <galilei.mail@gmail.com>
2024-03-30 16:00:48 -0700

    tiny fix

--- app/main.py
+++ app/main.py
@@ -7,10 +7,11 @@ from .sheet_exporter import GoogleSheetsCSVConverter
 
 class ScrapeFlow(object):
 
-    def __init__(self, parallel=1):
+    def __init__(self):
         google_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
         google_sheet_name = os.getenv('GOOGLE_SHEET_NAME')
 
+        self.max_cases = int(os.getenv('MAX_CASES', 10))
         self.sheet_exporter = GoogleSheetsCSVConverter(google_credentials, google_sheet_name)
         self.pipeline = HearingScraperPipeline()
         self.case_scraper = CaseScraper()
@@ -28,6 +29,7 @@ class ScrapeFlow(object):
         print("Start scraping hearing schedule")
         self.results = self.pipeline.get_latest_data()
         self.populate_cases()
+        self.dump_to_csv()
         self.publish_speardsheet()
         print("Finish scraping hearing schedule")
 
@@ -40,18 +42,20 @@ class ScrapeFlow(object):
         cdcr_numbers = self.results['CDC#'].unique()
         past_actions = []
         cases_details = []
+
         for i, cdcr_number in enumerate(cdcr_numbers):
             print(f"Scraping case details for {i} / {len(cdcr_numbers)}")
             case_details, case_past_actions = self.case_scraper.get_case_details(cdcr_number)
             past_actions.append(case_past_actions)
             cases_details.append(case_details)
 
-            if i > 10:
+            if i > self.max_cases:
                 break
 
         print("Finish scraping cases details")
         self.past_actions = pd.concat(past_actions, ignore_index=True)
         self.results = pd.merge(self.results, pd.DataFrame(cases_details), how='left', on='CDC#')
+
     def dump_to_csv(self):
         if isinstance(self.results, pd.DataFrame):
             self.results.to_csv('results.csv')
@@ -65,7 +69,6 @@ class ScrapeFlow(object):
         :return:
         """
         print("Publishing results to google sheet")
-        pass
 
 
 def main():
