17eb8e9806da2d93a2fc0b58c87e02bc8b4cecf0
Piyush <puropiyush@gmail.com>
2024-03-30 14:23:21 -0700

    new udpate

--- app/hearing_scraper/main.py
+++ app/hearing_scraper/main.py
@@ -4,66 +4,92 @@ from enum import Enum
 from urllib.request import urlopen
 from ssl import SSLError, CertificateError
 from urllib.error import HTTPError, URLError
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 property_file_path = "scrapper.yaml"
 h3_id_property_key = "h3_id"
 h3_string_property_key = "h3_string"
 
-
 class URLType(Enum):
     HEARING_SCHEDULE = "hearing-schedules"
     HEARING_RESULTS = "hearing-results"
 
+class SR_SCRAPER:
 
-def get_property_value(type: URLType, key: str):
-    # Read the config file and return the value for key
-    try:
-        with open(property_file_path, "r") as file:
-            config = yaml.safe_load(file)
-    except Exception as e:
-        print("Error while reading config file: " + str(e))
-        raise e
-    try:
-        return config[type.value][key]
-    except Exception as e:
-        print(config)
-        print("Error fetching key value: " + str(e))
-        raise e
+    def getData(self, base_url):
+        response = requests.get(base_url)
+        html = response.content
+        soup = BeautifulSoup(html, 'lxml')
 
+        table = soup.find('table', class_='has-fixed-layout')
+        table_rows = soup.find_all('tr')[1:]
 
-def get_hearing_schedule_base_url():
-    return get_property_value(URLType.HEARING_SCHEDULE, "url")
+        header = []
+        header = soup.find_all('th')
+        header = [h.text.strip() for h in header]
 
+        data = []
+        for row in table_rows:
+            cols = row.find_all('td')
+            cols = [col.text.strip() for col in cols]
+            data.append(cols)
 
-def get_hearing_results_base_url():
-    return get_property_value(URLType.HEARING_RESULTS, "url")
+        df = pd.DataFrame(data, columns=header)
+        return df
 
+    def get_property_value(self, type: URLType, key: str):
+        # Read the config file and return the value for key
+        try:
+            with open(property_file_path, "r") as file:
+                config = yaml.safe_load(file)
+        except Exception as e:
+            print("Error while reading config file: " + str(e))
+            raise e
+        try:
+            return config[type.value][key]
+        except Exception as e:
+            print(config)
+            print("Error fetching key value: " + str(e))
+            raise e
+        
+    def get_property(self, domain, property_name):
+        return self.get_property_value(domain, property_name)
 
-def process_url(url):
-    try:
-        url_obj = urlopen(url)
-        if url_obj.status == 200 and 'text/html' in url_obj.getheader('Content-Type'):
-            print("Crawled URL:" + url)
-            cur_page = url_obj.read().decode('utf-8')
-            parsed_content = HTMLParser(cur_page)
-            return parsed_content
-    except(HTTPError, TimeoutError, URLError, SSLError, CertificateError, UnicodeDecodeError) as e1:
-        print("Exception while crawling URL:\n" + str(e1))
-    except Exception as e:
-        print("Generic exception while crawling URL:\n" + str(e))
-    return None
-
-
-def get_monthly_urls_for_hearing_schedules():
-    base_url = get_hearing_schedule_base_url()
-    page_contents = process_url(base_url)
-    return page_contents.get_schedule_links_following_header_string(
-        id=get_property_value(URLType.HEARING_SCHEDULE, h3_id_property_key),
-        innerString=get_property_value(URLType.HEARING_SCHEDULE, h3_string_property_key))
+    def get_hearing_schedule_base_url(self):
+        return self.get_property_value(URLType.HEARING_SCHEDULE, "url")
 
 
-def get_monthly_urls_for_hearing_results():
-    base_url = get_hearing_results_base_url()
-    page_contents = process_url(base_url)
-    return page_contents.get_schedule_links_following_header_string(
-        innerString=get_property_value(URLType.HEARING_RESULTS, h3_string_property_key))
+    def get_hearing_results_base_url(self):
+        return self.get_property_value(URLType.HEARING_RESULTS, "url")
+
+
+    def process_url(self, url):
+        try:
+            url_obj = urlopen(url)
+            if url_obj.status == 200 and 'text/html' in url_obj.getheader('Content-Type'):
+                print("Crawled URL:" + url)
+                cur_page = url_obj.read().decode('utf-8')
+                parsed_content = HTMLParser(cur_page)
+                return parsed_content
+        except(HTTPError, TimeoutError, URLError, SSLError, CertificateError, UnicodeDecodeError) as e1:
+            print("Exception while crawling URL:\n" + str(e1))
+        except Exception as e:
+            print("Generic exception while crawling URL:\n" + str(e))
+        return None
+
+
+    def get_monthly_urls_for_hearing_schedules(self):
+        base_url = self.get_hearing_schedule_base_url()
+        page_contents = self.process_url(base_url)
+        return page_contents.get_schedule_links_following_header_string(
+            id=self.get_property_value(URLType.HEARING_SCHEDULE, h3_id_property_key),
+            innerString=self.get_property_value(URLType.HEARING_SCHEDULE, h3_string_property_key))
+
+
+    def get_monthly_urls_for_hearing_results(self):
+        base_url = self.get_hearing_results_base_url()
+        page_contents = self.process_url(base_url)
+        return page_contents.get_schedule_links_following_header_string(
+            innerString= self.get_property_value(URLType.HEARING_RESULTS, h3_string_property_key))
--- app/hearing_scraper/table_scraper.py
+++ app/hearing_scraper/table_scraper.py
@@ -1,32 +1,20 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
+scheduleUrl = "https://www.cdcr.ca.gov/bph/2023/01/25/february-2024-hearing-calendar/"
+resultUrl = "https://www.cdcr.ca.gov/bph/2024/03/20/hearing-results-february-2024/"
+
+df1 = getData(scheduleUrl)
+df2 = getData(resultUrl)
+
+df1 = df1[['CDC#','HEARING TYPE', 'PANEL', 'HEARING LOCATION', 'HEARING METHOD', 'SCHEDULED DATE', 'HEARING TIME']]
+df2  = df2[['CDC#', 'SCHEDULED DATE', 'OFFENDER NAME', 'HEARING METHOD', 'COUNTY OF COMMITMENT', 'GOV CODE', 'HEARING TYPE', 'RESULT']]
+
+merge_columns = ['CDC#', 'SCHEDULED DATE']
+for col in df1.columns:
+    if col in df2.columns and col not in merge_columns:
+        df1.rename(columns={col: col + '(SCHEDULE TABLE)'}, inplace=True)
+        df2.rename(columns={col: col + '(RESULT TABLE)'}, inplace=True)
+
+merged_data = pd.merge(df1, df2, on=merge_columns, how='outer')
 
 
-def getData(url):
-    base_url = url
-    response = requests.get(base_url)
-    html = response.content
-    soup = BeautifulSoup(html, 'lxml')
 
-    table = soup.find('table', class_='has-fixed-layout')
-    table_rows = soup.find_all('tr')[1:]
-
-    header = []
-    header = soup.find_all('th')
-    header = [h.text.strip() for h in header]
-
-    data = []
-    for row in table_rows:
-        cols = row.find_all('td')
-        cols = [col.text.strip() for col in cols]
-        data.append(cols)
-
-    df = pd.DataFrame(data, columns=header)
-    return df
-
-
-# gets the  table data in pandas datframe from the url
-url = "https://www.cdcr.ca.gov/bph/2024/03/20/hearing-results-february-2024/"
-data = getData(url)
-print(data)
+print(merged_data.columns)
